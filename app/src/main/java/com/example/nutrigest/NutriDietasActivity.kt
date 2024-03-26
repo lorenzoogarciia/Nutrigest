ef5ddb64a15590c0d515bc0e2a7d9ba01b24dd1b
Lorenzo Garcia <lorenzogarciagarcia03@gmail.com>
2024-03-26 19:51:48 +0100

    Actualización Activities Nutricionistas

--- app/src/main/java/com/example/nutrigest/NutriDietasActivity.kt
+++ app/src/main/java/com/example/nutrigest/NutriDietasActivity.kt
@@ -1,69 +1,69 @@
 package com.example.nutrigest
 
-
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.firestore.FirebaseFirestore
 
-class NutriHomeActivity : AppCompatActivity(),  NavigationView.OnNavigationItemSelectedListener {
+class NutriDietasActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     private lateinit var drawer: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
-
-    @SuppressLint("WrongViewCast")
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nutrihome)
+        setContentView(R.layout.activity_nutri_dietas)
 
-        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_nutrihome)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_nutridietas)
         setSupportActionBar(toolbar)
 
-        drawer = findViewById(R.id.drawer_nutrihome)
+        drawer = findViewById(R.id.drawer_nutridietas)
         toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigationdrawer_nutrihome_open, R.string.navigationdrawer_nutrihome_close)
         drawer.addDrawerListener(toggle)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
 
-        val navigationView: NavigationView = findViewById(R.id.nav_view_nutricionistas)
+        val navigationView: NavigationView = findViewById(R.id.nav_view_nutridietas)
         navigationView.setNavigationItemSelectedListener(this)
 
         val bundle = intent.extras
-        val mail: String?  = bundle?.getString("mail")
+        val email: String?  = bundle?.getString("mail")
 
-        val txtNutriHome = findViewById<TextView>(R.id.txt_nutrihome)
+        val txt_nutridietas = findViewById<TextView>(R.id.txt_nutridietas)
     }
-
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.nav_nutrihome_one -> {
+                val homeIntent = Intent(this, NutriHomeActivity::class.java).apply{
+                    putExtra("mail", intent.getStringExtra("mail"))}
+                startActivity(homeIntent)
                 drawer.closeDrawer(GravityCompat.START)
                 Toast.makeText(this, "Inicio", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_nutrihome_two -> {
-                val perfilIntent = Intent(this, NutriPerfilActivity::class.java).apply{
-                    putExtra("mail", intent.getStringExtra("mail"))}
+                val perfilIntent = Intent(this, NutriPerfilActivity::class.java).apply {
+                    putExtra("mail", intent.getStringExtra("mail"))
+                }
                 startActivity(perfilIntent)
                 drawer.closeDrawer(GravityCompat.START)
                 Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_nutrihome_three -> {
-                val dietasIntent = Intent(this, NutriDietasActivity::class.java).apply {
-                    putExtra("mail", intent.getStringExtra("mail"))
-                }
-                startActivity(dietasIntent)
                 drawer.closeDrawer(GravityCompat.START)
                 Toast.makeText(this, "Dietas", Toast.LENGTH_SHORT).show()
             }
--- app/src/main/java/com/example/nutrigest/NutriPerfilActivity.kt
+++ app/src/main/java/com/example/nutrigest/NutriPerfilActivity.kt
@@ -1,61 +1,59 @@
 package com.example.nutrigest
 
-
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.firestore.FirebaseFirestore
 
-class NutriHomeActivity : AppCompatActivity(),  NavigationView.OnNavigationItemSelectedListener {
+class NutriPerfilActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     private lateinit var drawer: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
-
-    @SuppressLint("WrongViewCast")
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nutrihome)
+        setContentView(R.layout.activity_nutri_perfil)
 
-        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_nutrihome)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_perfilnutricionistas)
         setSupportActionBar(toolbar)
 
-        drawer = findViewById(R.id.drawer_nutrihome)
+        drawer = findViewById(R.id.drawer_perfilnutricionistas)
         toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigationdrawer_nutrihome_open, R.string.navigationdrawer_nutrihome_close)
         drawer.addDrawerListener(toggle)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
 
-        val navigationView: NavigationView = findViewById(R.id.nav_view_nutricionistas)
+        val navigationView: NavigationView = findViewById(R.id.nav_view_perfilnutricionistas)
         navigationView.setNavigationItemSelectedListener(this)
 
         val bundle = intent.extras
         val mail: String?  = bundle?.getString("mail")
 
-        val txtNutriHome = findViewById<TextView>(R.id.txt_nutrihome)
-    }
+        val txt_perfilnutricionistas = findViewById<TextView>(R.id.txt_perfilnutricionistas)
 
+    }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.nav_nutrihome_one -> {
+                val homeIntent = Intent(this, NutriHomeActivity::class.java).apply{
+                    putExtra("mail", intent.getStringExtra("mail"))}
+                startActivity(homeIntent)
                 drawer.closeDrawer(GravityCompat.START)
                 Toast.makeText(this, "Inicio", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_nutrihome_two -> {
-                val perfilIntent = Intent(this, NutriPerfilActivity::class.java).apply{
-                    putExtra("mail", intent.getStringExtra("mail"))}
-                startActivity(perfilIntent)
                 drawer.closeDrawer(GravityCompat.START)
                 Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show()
             }
