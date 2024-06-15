9b456efe68cf5f7fcefdcfab6d6afcbf7131b241
Lorenzo Garcia <lorenzogarciagarcia03@gmail.com>
2024-06-15 15:29:19 +0200

    SerNutricionistaActivity

--- app/src/main/java/com/example/nutrigest/SerNutricionistaActivity.kt
+++ app/src/main/java/com/example/nutrigest/SerNutricionistaActivity.kt
@@ -12,30 +12,20 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import com.google.api.client.extensions.android.http.AndroidHttp
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
-import com.google.api.client.json.gson.GsonFactory
-import com.google.api.client.util.Base64
-import com.google.api.services.gmail.Gmail
-import com.google.api.services.gmail.model.Message
+
 import java.io.ByteArrayOutputStream
 import java.util.Properties
-import javax.mail.Session
-import javax.mail.internet.InternetAddress
-import javax.mail.internet.MimeMessage
+
 
 
 class SerNutricionistaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_ser_nutricionista)
 
         //Setup
@@ -79,56 +69,11 @@ class SerNutricionistaActivity : AppCompatActivity() {
 
     //Función para enviar correo
     private fun enviarMail(mailNutricionista: String) {
-        Log.d("enviarMail", "Mail: $mailNutricionista")
-        val credential = getServiceAccountCredential()
-        val service = Gmail.Builder(AndroidHttp.newCompatibleTransport(), GsonFactory(), credential)
-            .setApplicationName("NutriGest")
-            .build()
 
-        val props = Properties()
-        val session = Session.getDefaultInstance(props, null)
 
-        try {
-            val mimeMessage = MimeMessage(session).apply {
-                setFrom(InternetAddress("nutrigest@nutrigest.com"))
-                addRecipient(javax.mail.Message.RecipientType.TO, InternetAddress(mailNutricionista))
-                subject = "Solicitud para ser nutricionista"
-                setText("Muy buenas, su solicitud para ser nutricionista ha sido recibida. \n" +
-                        "En breve nos pondremos en contacto con usted para continuar con el proceso.")
-            }
-
-            val buffer = ByteArrayOutputStream()
-            mimeMessage.writeTo(buffer)
-            val rawMessage = Base64.encodeBase64URLSafeString(buffer.toByteArray())
-
-            val message = Message().apply {
-                raw = rawMessage
-            }
-
-            service.users().messages().send("me", message).execute()
-
-            runOnUiThread {
-                Toast.makeText(this, "Mail enviado correctamente", Toast.LENGTH_SHORT).show()
-            }
-
-        }catch (e: Exception){
-            e.printStackTrace()
-            runOnUiThread {
-                Log.e("ERROR", "Error al enviar mail: ${e.toString()}")
-                showAlert("Error al enviar el correo: ${e.message}")
-            }
-        }
     }
 
-    //Función para obtener las credenciales de la cuenta de Google
-    private fun getServiceAccountCredential(): GoogleCredential {
-        Log.d("getServiceAccountCredential", "Obteniendo credenciales")
-        val inputStream = resources.openRawResource(R.raw.credentials)
-        Log.d("getServiceAccountCredential", "InputStream: $inputStream")
-        return GoogleCredential.fromStream(inputStream)
-            .createScoped(listOf("https://www.googleapis.com/auth/gmail.send"))
-            .createDelegated("nutrigest@nutrigest.com")
-    }
+
 
         fun showAlert(message: String){
         val builder = AlertDialog.Builder(this)
