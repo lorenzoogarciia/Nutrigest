5d6675a881ce2419f23cd8bed475c38b37e82563
Lorenzo Garcia <lorenzogarciagarcia03@gmail.com>
2024-06-17 16:37:54 +0200

    Solucionado bug visual al ver dietas en la comida "Media Mañana"

--- app/src/main/java/com/example/nutrigest/clases/Dietas.kt
+++ app/src/main/java/com/example/nutrigest/clases/Dietas.kt
@@ -1,9 +1,11 @@
 package com.example.nutrigest.clases
 
+import com.google.firebase.firestore.PropertyName
+
 data class Dietas(
     val Almuerzo: List<Alimentos> = listOf(),
     val Cena: List<Alimentos> = listOf(),
     val Desayuno: List<Alimentos> = listOf(),
-    val MediaManana: List<Alimentos> = listOf(),
+    @get:PropertyName("Media mañana") @set:PropertyName("Media mañana") var MediaManana: List<Alimentos> = listOf(),
     val Merienda: List<Alimentos> = listOf()
 )
